83f4adc4f0a9130fa81a3092f2cb43b820d9b3ae
mariorr008 <marioalr8@gmail.com>
2026-03-17 18:29:35 -0500

    feat: se añadio funcion eliminar usuario

--- GestionarUsuarios.py
+++ GestionarUsuarios.py
@@ -1,3 +1,7 @@
 def agredarUsuario():
 	usuario=input('Ingrese el nombre del usuario que desea añadir')
 	print(usuario)
+
+def eliminarUsuario():
+	usuario=input('Ingrese el nombre del usuario que desea eliminar')
+	print(usuario)
